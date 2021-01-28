6bc8e24d0c624f97a5dfdce3df7a59c18b833a97
kaelwebdev <cdcp-1795@hotmail.com>
2021-01-28 00:20:28 -0800

    improving file UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,30 +1,34 @@
 import sha1 from 'sha1';
 import dbClient from '../utils/db';
 
-// manipulation of user collection in database
+export default class UsersController {
+  // manipulation of user collection in database
 
-export const findUser = async (user) => dbClient.db.collection('users').findOne(user);
+  static async findUser(user) {
+    return dbClient.db.collection('users').findOne(user);
+  }
 
-export const createUser = async (email, password) => {
-  await dbClient.db.collection('users').insertOne({ email, password });
+  static async createUser(email, password) {
+    await dbClient.db.collection('users').insertOne({ email, password });
 
-  const newUser = await dbClient.db.collection('users').findOne({ email });
+    const newUser = await dbClient.db.collection('users').findOne({ email });
 
-  return { id: newUser._id, email };
-};
+    return { id: newUser._id, email };
+  }
 
-// user control functions
+  // user control functions
 
-export const newUser = async (req, res) => {
-  const { email, password } = req.body;
+  static async newUser(req, res) {
+    const { email, password } = req.body;
 
-  if (!email) return res.status(400).json({ error: 'Missing email' });
-  if (!password) return res.status(400).json({ error: 'Missing password' });
+    if (!email) return res.status(400).json({ error: 'Missing email' });
+    if (!password) return res.status(400).json({ error: 'Missing password' });
 
-  let user = await findUser({ email });
-  if (user) return res.status(400).json({ error: 'Already exist' });
+    let user = await UsersController.findUser({ email });
+    if (user) return res.status(400).json({ error: 'Already exist' });
 
-  user = await createUser(email, sha1(password));
+    user = await UsersController.createUser(email, sha1(password));
 
-  return res.status(201).send(user);
-};
+    return res.status(201).send(user);
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
 
 import { getStatus, getStats } from '../controllers/AppController';
-import { newUser } from '../controllers/UsersController';
+import UsersController from '../controllers/UsersController';
 
 const router = express.Router();
 
 router.get('/status', getStatus);
 router.get('/stats', getStats);
-router.post('/users', newUser);
+router.post('/users', UsersController.newUser);
 
 export default router;
